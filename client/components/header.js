5b5827d6cc81b04ae30270f8423c908ec9a0fc00
Michael Cuccia <michael.cuccia@egplusww.com>
2022-01-20 10:51:59 -0500

    added icon library

--- client/components/header.js
+++ client/components/header.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
+import { MdHome, MdMenuBook, MdContactMail, MdContactSupport, MdReadMore, MdPlaylistAddCheck } from "react-icons/md";
 import * as palette from '../constants/palette';
 import Logo from './logo';
 
 const Main = styled.div`
     width: 100%;
     display: flex;
+    font-family: sans-serif;
 `;
 
 const LogoContainer = styled.div`
@@ -25,6 +27,34 @@ const Tab = styled.div`
     align-items: center;
     background: ${palette.GRAYSWIRL};
     color: black;   
+
+    .text {
+        padding-top: 10px;
+    }
+`;
+
+const EndContainer = styled.div`
+    width: 20%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding-left: 15px;
+    background: ${palette.GRAYSWIRL};
+    color: black;
+`;
+
+const IconWrapper = styled.span`
+    color: ${palette.WUGOLD};
+    font-size: 28px;
+    display: flex;
+    align-items: center;
+
+    .text {
+        color: black;
+        font-size: 16px;
+        padding-left: 15px;
+    }
 `;
 
 export default function header() {
@@ -33,13 +63,48 @@ export default function header() {
         <LogoContainer>
             <Logo />
         </LogoContainer>
-        <Tab>Home</Tab>
-        <Tab>Contact</Tab>
-        <Tab>About</Tab>
-        <Tab>Support</Tab>
-        <LogoContainer>
-            Hello World
-        </LogoContainer>
+        <Tab>
+            <IconWrapper>
+                <MdHome/>
+            </IconWrapper>     
+            <p className="text">
+                HOME
+            </p>   
+        </Tab>
+        <Tab>
+            <IconWrapper>
+                <MdContactMail />
+            </IconWrapper> 
+            <p className="text">
+                CONTACT
+            </p> 
+        </Tab>
+        <Tab>
+            <IconWrapper>
+                <MdMenuBook />
+            </IconWrapper>
+            <p className="text">
+                About
+            </p> 
+        </Tab>
+        <Tab>
+            <IconWrapper>
+                <MdContactSupport />
+            </IconWrapper>
+            <p className="text">
+                SUPPORT
+            </p> 
+        </Tab>
+        <EndContainer>
+            <IconWrapper>
+                <MdReadMore />
+                <span className="text">Learn More About our Faculty</span>
+            </IconWrapper>
+            <IconWrapper>
+                <MdPlaylistAddCheck />
+                <span className="text">Get the latest news</span>
+            </IconWrapper> 
+        </EndContainer>
     </Main>
   ) 
 }
